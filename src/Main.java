d43d5a01e3b62403adcf41ceba3e3b835aed3a9c
PiscesYE <2690057415>
2019-07-19 08:52:23 +0800

    ok

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@ import sort.Sort;
 
 public class Main {
 
+    public static int anInt;
+
     public static void main(String[] args) {
-        int array[] = {1,15,18,22,2,4};
+        /*int array[] = {1,15,18,22,2,4};
         Sort.quickSort(array, 0, 5);
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
-        }
+        }*/
+        System.out.println(anInt);
     }
     //1,15,4,2,22,18
     //1,2,4,15,22,18 =》 strat = 1,j = 3;
